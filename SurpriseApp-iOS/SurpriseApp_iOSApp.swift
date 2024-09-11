041a0b58806c0f80dd826f17d5968b4c0b4c68ce
timi2506 <timschuchardt@icloud.com>
2024-09-11 20:46:52 +0200

    Add files via upload

---
+++ SurpriseApp-iOS/SurpriseApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurpriseApp_iOSApp.swift
+//  SurpriseApp-iOS
+//
+//  Created by Tim Schuchardt on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SurpriseApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurpriseApp-iOSTests/SurpriseApp_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurpriseApp_iOSTests.swift
+//  SurpriseApp-iOSTests
+//
+//  Created by Tim Schuchardt on 11.09.2024.
+//
+
+import Testing
+@testable import SurpriseApp_iOS
+
+struct SurpriseApp_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
